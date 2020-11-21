da90d6f02c13c5382f42ded187cd0d068827c856
rmamoonx <r.mamoonx@hotmail.com>
2020-11-21 17:52:53 +0400

    Fixed Api

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,39 +1,34 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function ProductCard(props){
-    return (
-        <div className="border mb-4 rounded overflow-hidden">
-            <Link to={`/products/${props.product.id}`}>
-                <div 
-                    style={{
-                        'backgroundImage': `url('${props.product.images[0].imageUrl}')`,
-                    }}
-                    className="w-full h-64 bg-blue bg-cover"
-                >
-                </div>
-            </Link>
-            <div className="p-3">
-                <h3 className="font-bold text-xl mb-3">
-                    <Link to={`/products/${props.product.id}`}>
-                        { props.product.name }
-                    </Link>    
-                </h3>
-                <div className="font-bold mb-3">
-                    $ { props.product.price }
-                </div>
-                <div className="mb-3">
-                    { props.product.description }
-                </div>
-                <Link 
-                    to={`/products/${props.product.id}`}
-                    className="bg-blue-500 text-white p-2 flex justify-center w-full"
-                >
-                    View
-                </Link>
-            </div>
-        </div>
-    )
+function ProductCard(props) {
+  return (
+    <div className="border mb-4 rounded overflow-hidden">
+      <Link to={`/products/${props.product.idMeal}`}>
+        <div
+          style={{
+            backgroundImage: `url('${props.product.strMealThumb}')`,
+          }}
+          className="w-full h-64 bg-blue bg-cover"
+        ></div>
+      </Link>
+      <div className="p-3">
+        <h3 className="font-bold text-xl mb-3">
+          <Link to={`/products/${props.product.idMeal}`}>
+            {props.product.strMeal}
+          </Link>
+        </h3>
+        <div className="font-bold mb-3"> {props.product.name}</div>
+        <div className="mb-3">{props.product.strInstructions}</div>
+        <Link
+          to={`/products/${props.product.idMeal}`}
+          className="bg-blue-500 text-white p-2 flex justify-center w-full"
+        >
+          View
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,58 +1,56 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import Loader from '../Components/Loader'
-import { useAxiosGet } from '../Hooks/HttpRequests'
+import React from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
+import { useAxiosGet } from "../Hooks/HttpRequests";
 
-function Product(){
-    const { id } = useParams()
-    // Create your own Mock API: https://mockapi.io/
-    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
-    
-    let product = useAxiosGet(url)
+function Product() {
+  const { id } = useParams();
+  // Create your own Mock API: https://mockapi.io/
+  const url = ` https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 
-    let content = null
+  let product = useAxiosGet(url);
 
-    if(product.error){
-        content = <div>
-            <div className="bg-blue-300 mb-2 p-3">
-                If you see this error. Please remember to create your own <a href="https://mockapi.io/">mock API</a>.
-            </div>
-            <div className="bg-red-300 p-3">
-                There was an error please refresh or try again later.
-            </div>
+  let content = null;
+
+  if (product.error) {
+    content = (
+      <div>
+        <div className="bg-blue-300 mb-2 p-3">
+          If you see this error. Please remember to create your own{" "}
+          <a href="https://mockapi.io/">mock API</a>.
         </div>
-    }
+        <div className="bg-red-300 p-3">
+          There was an error please refresh or try again later.
+        </div>
+      </div>
+    );
+  }
 
-    if(product.loading){
-        content = <Loader></Loader>
-    }
+  if (product.loading) {
+    content = <Loader></Loader>;
+  }
 
-    if(product.data){
-        content = 
+  if (product.data) {
+    content = (
+      <div>
+        <h1 className="text-2xl font-bold mb-3">
+          {product.data.meals[0].idMeal}
+        </h1>
         <div>
-            <h1 className="text-2xl font-bold mb-3">
-                {product.data.name}
-            </h1>
-            <div>
-                <img
-                    src={product.data.images[0].imageUrl}
-                    alt={product.data.name}
-                />
-            </div>
-            <div className="font-bold text-xl mb-3">
-                $ {product.data.price}
-            </div>
-            <div>
-                {product.data.description}
-            </div>
+          <img
+            src={product.data.meals[0].strMealThumb}
+            alt={product.data.meals[0].strMeal}
+          />
         </div>
-    }
+        <div className="font-bold text-xl mb-3">
+          {product.data.meals[0].strMeal}
+        </div>
+        <div>{product.data.meals[0].strCategory}</div>
+      </div>
+    );
+  }
 
-    return (
-        <div className="container mx-auto">
-            {content}
-        </div>
-    )
+  return <div className="container mx-auto">{content}</div>;
 }
 
-export default Product
+export default Product;
